f5b2308cf0fa825cf47bbf9fa38538c73bbaa444
RoyAkumu <jroakumu@ucdavis.edu>
2024-10-03 13:56:04 -0700

    Week 2 in Class Notes

---
+++ Week_2_Intro_to_R.R
@@ -0,0 +1,67 @@
+2*5
+4+(8*3)+2
+exp(1)
+exp(3)
+log(4)
+sqrt(4)
+
+?log
+log(2,4)
+log(4,2)
+log(x=2, base=4)
+log(base=4, x=2)
+x<-1
+rm(x)
+
+sqrt(exp(4))
+
+
+mynumber <- 6
+mynumber == 5
+
+mynumber == 6
+
+mynumber != 5
+
+mynumber > 7
+mynumber < 5
+
+othernumber <- -3
+
+mynumber * othernumber
+
+
+#object name completion
+
+numSamples <- 50
+
+num_samples <- 40
+rm(num_samples)
+
+
+log("a_word")
+
+log_of_word <- log("a_word")
+log_of_word
+
+
+log_of_negative <- log(-2)
+
+log_of_negative
+
+
+#challenge
+
+elephant1_kg <- 3492
+elephant2_lb < 7757
+elephant2_lb <- 7757
+
+elephant1_lb <- elephant1_kg *2.2
+elephant2_lb > elephant1_lb
+
+
+my elephants <- c(elephant1_lb, elephant2_lb)
+
+
+myelephants <- c(elephant1_lb, elephant2_lb)
+which(myelephants == max(myelephants))
