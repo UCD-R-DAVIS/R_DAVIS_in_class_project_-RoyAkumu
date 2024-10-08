ab391ffb399eff7ddb7af8c2cd7e7c60fc86c837
RoyAkumu <jroakumu@ucdavis.edu>
2024-10-08 14:17:11 -0700

    Vector Math, Project Management, Data types, Exploring vectors, Subsetting

--- Week_2_Intro_to_R.R
+++ Week_2_Intro_to_R.R
@@ -116,3 +116,105 @@ class(num_char)
 
 getwd()
 
+
+#project management
+
+
+getwd()
+
+# Relative file path
+
+  
+#Raw data considered read only file
+  
+  
+  
+#How R thinks about data ----
+
+## Vector and data Types ----
+
+### Create Objects ----
+
+####Use c to concatenate ie add multiple objects
+
+weight <- c(50, 73, 47, 2)
+
+weight
+### vector with text
+
+#surround the words with quotes - for character values
+animals <- c("mouse", horse, "rat")
+
+animals <- c("mouse", "horse", "rat")
+
+#explore the objects
+#class of vector
+
+#additional functions
+#class
+
+class(weight)
+
+class(animals)
+#length
+
+lenght(weight)
+
+length(weight)
+length(animals)
+
+#structure
+str(weight)
+
+str(animals)
+
+#change vector
+
+
+weight <- c(weight, 90) #adds to the end of the vector
+weight
+weight
+
+weight <- c(3, weight)
+
+weight
+
+
+#Subsetting
+
+##Extracting specific values out of vectors
+
+animals <- c("mouse", "rat", "dog", "cat")
+
+#use square brackets
+animals[2]
+
+#use concatenate function (c) to extract multiple values
+animals[c(2,3)]
+
+#create a new set of values 
+animals[c(2,3,2,3,2,3)]
+
+
+#conditional subsetting 
+##using logical vector subsetting
+
+weight <- c(14, 76, 81, 21, 46)
+
+weight[c(TRUE, TRUE, FALSE, FALSE, FALSE)]
+##specify the condition
+weight[weight > 50]
+weight > 50
+
+#multiple tests
+weight[weight > 50 | weight < 20]
+
+#or is denoted by the straight line as above
+
+# AND ---> & 
+# other symbols
+
+weight[weight >= 30]
+weight[weight >=46]
+
+weight[weight >= 46]
